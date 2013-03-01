5f6d2131ff35b9d7db808c40d7b388b0121a5eb4
mostlyclicks <carlos@mostlyclicks.com>
2013-02-28 21:05:12 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paparazzi::Application.config.secret_token = '63db2a946e0b8ffc745bc9e7dc2db7fadf829c2ff1785ed4cbd8547be55b09dd0b7afe38d42c57e20023201956618e5acd8a97f9b66a31706ed0454aacda2dd1'
